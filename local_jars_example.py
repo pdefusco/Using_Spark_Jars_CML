ea62a6e68dd2bd086abc72bbfa7c5bb07e5beecb
Paul de Fusco <pauldefusco@cloudera.com>
2023-03-02 20:22:58 +0000

    added extraClassPath vars

--- local_jars_example.py
+++ local_jars_example.py
@@ -1,10 +1,13 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder\
-        .appName("MySQL Connection")\
+        .appName("Sample Jars Application")\
         .config("spark.jars", "/opt/spark/examples/jars/spark-examples.jar,/opt/spark/examples/jars/scopt_2.12-3.7.1.jar")\
+        .config("spark.driver.extraClassPath", "/opt/spark/examples/jars/spark-examples.jar,/opt/spark/examples/jars/scopt_2.12-3.7.1.jar")\
+        .config("spark.executor.extraClassPath", "/opt/spark/examples/jars/spark-examples.jar,/opt/spark/examples/jars/scopt_2.12-3.7.1.jar")\
         .getOrCreate()
       
 spark.sparkContext.getConf().getAll()
 
-spark.stop()
+#spark.stop()
+
